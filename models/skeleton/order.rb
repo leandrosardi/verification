a95501f1c0832f84667e88d38d606181504d46e9
leandrosardi <leandro.sardi@expandedventure.com>
2022-11-29 18:09:08 -0300

    leandrosardi/leads#95

--- models/skeleton/order.rb
+++ models/skeleton/order.rb
@@ -2,7 +2,7 @@ module BlackStack
     module Verification
         class Order < Sequel::Model(:vrf_order)
             many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
-            many_to_one :export, :class=>:'Leads::FlExport', :key=>:id_export
+            many_to_one :export, :class=>:'BlackStack::LeadsExport', :key=>:id_export
         end # class Order
     end # Verification
 end # BlackStack
